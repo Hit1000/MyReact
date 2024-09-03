2a7f3c945a541aa0ced8a9bca9e7225a562e81aa
Hit1000 <hiteshsingla209@gmail.com>
2024-09-03 12:29:23 +0530

    cursor

--- 01_viteBasic/src/App.jsx
+++ 01_viteBasic/src/App.jsx
@@ -1,17 +1,55 @@
-
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "./App.css";
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [cursorVar, setCursorVar] = useState("default");
 
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.addEventListener("mousemove", mouseMove);
+    };
+  }, [position, setPosition]);
+
+  const variants = {
+    default: {
+      x: position.x - 16,
+      y: position.y - 16,
+    },
+    text: {
+      x: position.x - 16,
+      y: position.y - 16,
+      scale: 6,
+      backgroundColor: "yellow",
+      mixBlendMode: "difference"
+    }
+  };
+
+  const textenter = () => setCursorVar('text');
+  const textleave = () => setCursorVar('default');
 
   return (
     <>
-      <h1>hello</h1>
-      <h2>counter = 5</h2>
-      <button>increase</button>
-      <br />
-      <button>decrease</button>
+      <div className="App">
+        <h1 onMouseEnter={textenter} onMouseLeave={textleave} className="title">
+          hello WORLD
+        </h1>
+        <motion.div
+          className="cursor"
+          variants={variants}
+          animate={cursorVar}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 09_themeswicher/src/App.jsx
+++ 09_themeswicher/src/App.jsx
@@ -18,21 +18,30 @@ function App() {
     document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
 
+  const [cursorX, setCursorX] = useState(0);
+  const [cursorY, setCursorY] = useState(0);
+  window.addEventListener('mousemove', (e) => {
+    setCursorX(e.pageX - 10);
+    setCursorY(e.pageY- 10);
+  })
+
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-      <div className="flex flex-wrap min-h-screen items-center">
+
+      <div className="flex flex-wrap min-h-screen items-center ">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             {/* theme btn */}
             <Button />
           </div>
 
-          <div className="w-full max-w-l gap-2 mx-auto flex">
+          <div className="w-full max-w-l gap-2 mx-auto flex hover-item" id="card">
             {/* card */}
             <Card />
             <Card />
             <Card />
           </div>
+          <div className='cursor' style={{left:cursorX+'px', top:cursorY+'px'}}></div>
         </div>
       </div>
     </ThemeProvider>
