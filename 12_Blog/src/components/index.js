be6dcfe9dc613106801ad5535798960d750eb5b8
HiT <hiteshsingla209@gmail.com>
2024-10-09 00:33:43 +0530

    8.4

--- 12_Blog/src/components/index.js
+++ 12_Blog/src/components/index.js
@@ -1,4 +1,8 @@
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
 
-export { Header, Footer }
+export { Header, Footer, Logo, Container, LogoutBtn, Input };
