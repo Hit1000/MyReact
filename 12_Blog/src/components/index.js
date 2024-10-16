d6955634d30901ee2fbedf995882e722c9f027c8
HiT <hiteshsingla209@gmail.com>
2024-10-17 02:11:26 +0530

    8.6

--- 12_Blog/src/components/index.js
+++ 12_Blog/src/components/index.js
@@ -4,5 +4,10 @@ import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
+import Select from './Select';
+import Button from './Button';
+import Signup from './Signup';
+import Login from './Login';
+import RTE from "./RTE";
 
-export { Header, Footer, Logo, Container, LogoutBtn, Input };
+export { Header, Footer, Logo, Container, LogoutBtn, Input, Select, Button, Signup, Login, RTE };
