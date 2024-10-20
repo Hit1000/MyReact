93c2ddd8133bbde9019c651bc3b1dfcade2ad4f3
HiT <hiteshsingla209@gmail.com>
2024-10-20 18:18:30 +0530

    8.7

--- 12_Blog/src/components/index.js
+++ 12_Blog/src/components/index.js
@@ -4,10 +4,28 @@ import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
-import Select from './Select';
-import Button from './Button';
-import Signup from './Signup';
-import Login from './Login';
+import Select from "./Select";
+import Button from "./Button";
+import Signup from "./Signup";
+import Login from "./Login";
 import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
-export { Header, Footer, Logo, Container, LogoutBtn, Input, Select, Button, Signup, Login, RTE };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Input,
+  Select,
+  Button,
+  Signup,
+  Login,
+  RTE,
+  PostForm,
+  PostCard,
+  AuthLayout
+};
